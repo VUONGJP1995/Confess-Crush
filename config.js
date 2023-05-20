92fe0342a75835e56c2f16c57bd522d48fc05096
VUONGJP1995 <134139783+VUONGJP1995@users.noreply.github.com>
2023-05-20 19:54:08 +0900

    Lời tỏ tình dễ thương

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Kính thưa phu nhân tương lai",
+    introTitle: 'Quỳnh Gaby',
+    introDesc: `Nếu anh là nhà thơ anh sẽ viết tặng em những vần thơ cháy bỏng,
+    Nếu anh là nhạs sỹ anh sẽ viết tặng em những bản tình ca bất hủ,
+    Nếu anh là lập trình viên anh sẽ viết nên những đoạn mã ngập tràn hương vị tình yêu,
+    Và nếu anh có thể anh sẽ làm tất cả vì em.`,
+    btnIntro: '^^ahihi đồ ngốc^^',
+    title: 'Phải chăng chúng ta chính là định mệnh 🥰',
+    desc: 'Làm người yêu anh nhé Quỳnh Gaby ',
+    btnYes: 'dạ anh <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'em có điều gì muốn nói với anh không?',
+    btnReply: 'Gửi anh <3',
+    reply: 'Em cũng yêu anh <33333333',
+    mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'tối nay chúng ta hẹn hò nhé.',
+    btnAccept: 'dạ anh <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
